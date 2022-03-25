555830b26ae90b00c8a8eb532f511253330202ad
IvanSerranoGit <contacto.ivanserrano@gmail.com>
2022-03-25 14:24:59 -0600

    componentes en react

---
+++ src/componentes/Texto.js
@@ -0,0 +1,5 @@
+const texto = 'Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando en los cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
+
+if (texto.includes('freeCodeCamp')) {
+    
+}
